fc69ccae9bb8dc63290426fc606a755efa239b2d
Robin <rgao@semios.com>
2022-07-05 15:16:53 -0700

    build required tar file for target gateway

--- LnProvision-Over-Ssh.sh
+++ LnProvision-Over-Ssh.sh
@@ -9,12 +9,13 @@ echo "+------------------------------------------------------------+"
 echo "|                     Start provisioning                     |"
 echo "+------------------------------------------------------------+"
 
+#Create the crendential package and config package for target gateway
+tar -czvf Crendential.tar.gz Crendential
+tar -czvf LnConfig.tar.gz LnConfig
+
 gtwy_ip=$1
 password="root"
 port="22"
 echo "Logging into the LN gateway @$gtwy_ip"
-
-tar -czvf Crendential.tar.gz Crendential
-
 # remove ssh key from the provisioining computer
 ssh-keygen -f ~/.ssh/known_hosts -R "[$gtwy_ip]:22"
