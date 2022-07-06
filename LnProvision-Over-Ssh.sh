1a876b038a653dc0b22d33d7c29846663612f60e
Robin <rgao@semios.com>
2022-07-05 19:09:14 -0700

    added check login, ssh, scp file without pass

--- LnProvision-Over-Ssh.sh
+++ LnProvision-Over-Ssh.sh
@@ -9,13 +9,46 @@ echo "+------------------------------------------------------------+"
 echo "|                     Start provisioning                     |"
 echo "+------------------------------------------------------------+"
 
-#Create the crendential package and config package for target gateway
+gtwy_ip=$1
+
+password=$SEMIOS_PROVISIONER_GWLN_PASSWORD
+port=$SEMIOS_PROVISIONER_GWLN_PORT
+echo "pass is $password"
+echo "port is $port"
+
+if [ ! -f Crendential/client.crt ]; then
+    echo "The crendential file 'client.crt' must exist in directory. see README prerequisties!"
+    exit 1
+fi
+
+if [ ! -f Crendential/client.key ]; then
+    echo "The crendential file 'client.key' must exist in directory. see README prerequisties!"
+    exit 1
+fi
+
+if [ ! -f Crendential/server.trust ]; then
+    echo "The crendential file 'server.trust' must exist in directory. see README prerequisties!"
+    exit 1
+fi
+
+#remove old gz files
+rm Crendential.tar.gz
+rm LnConfig.tar.gz
+
+#Create latest crendential package and config package for target gateway
 tar -czvf Crendential.tar.gz Crendential
 tar -czvf LnConfig.tar.gz LnConfig
 
-gtwy_ip=$1
-password="root"
-port="22"
 echo "Logging into the LN gateway @$gtwy_ip"
-# remove ssh key from the provisioining computer
-ssh-keygen -f ~/.ssh/known_hosts -R "[$gtwy_ip]:22"
+ssh-keygen -f ~/.ssh/known_hosts -R "$gtwy_ip"
+sshpass -p $password ssh -p $port -q -o ConnectTimeout=15 root@$gtwy_ip -oStrictHostKeyChecking=accept-new exit
+if [ ! $? -eq 0 ]; then
+
+  echo "Unable to connect over ssh to gateway @ IP: $gtwy_ip"
+  exit
+
+fi
+
+sshpass -p $password scp -P $port Crendential.tar.gz root@$gtwy_ip:/tmp
+sshpass -p $password scp -P $port LnConfig.tar.gz root@$gtwy_ip:/tmp
+sshpass -p $password scp -P $port LN-Remote-Update.sh root@$gtwy_ip:/tmp
